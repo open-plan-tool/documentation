8b523a58b86f884813d3b233d89bf6f996a98a1e
valkalais <valkalais@intracom-telecom.com>
2020-10-22 14:51:49 +0300

    Selectize operational!

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import timeseries_chart_jsonview, scenario_visualize_results, scenario_available_results
+from .views import *
 
 urlpatterns = [
     path('scenario/results/get_chart/<int:scen_id>', timeseries_chart_jsonview, name='scenario_json_chart'),
     path('scenario/results/visualize/<int:scen_id>', scenario_visualize_results, name='scenario_visualize_results'),
+    path('scenario/results/request/<int:scen_id>', scenario_request_results, name='scenario_request_results'),
     path('scenario/results/available/<int:scen_id>', scenario_available_results, name='scenario_available_results'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -44,39 +44,85 @@ def scenario_available_results(request, scen_id):
     ]
 
     # Generate available asset type JSON
-    asset_type_json = [
+    asset_name_json = [
         [
             {
                 'assetCategory': asset_category,
-                'assetType': asset_type
+                'assetName': asset_name
             }
-            for asset_type in dict_values[asset_category].keys()
+            for asset_name in dict_values[asset_category].keys()
         ]
         for asset_category in asset_category_list
     ]
 
-    response_json = {'options': asset_type_json, 'optgroups': asset_category_json}
+    response_json = {'options': asset_name_json, 'optgroups': asset_category_json}
 
     return JsonResponse(response_json, status=200, content_type='application/json')
 
 
 @login_required
-@require_http_methods(["GET", "POST"])
+@json_view
+@require_http_methods(["GET"])
+def scenario_request_results(request, scen_id):
+    scenario = get_object_or_404(Scenario, pk=scen_id)
+
+    if scenario.project.user != request.user:
+        return HttpResponseForbidden()
+
+    with open('static/tempFiles/json_with_results.json') as json_file:
+        dict_values = json.load(json_file)
+
+    asset_name_list = request.GET.get('assetNameList').split(',')
+
+    asset_category_list = [
+        'energyProviders', 'energyConsumption', 'energyConversion', 'energyStorage', 'energyProduction']
+
+    # Asset category to asset type
+    asset_name_to_category = {
+            asset_name: asset_category
+            for asset_category in asset_category_list
+            for asset_name in dict_values[asset_category]
+        }
+
+
+
+    # Generate results JSON per asset name
+    results_json = [
+        {
+            'xAxis':
+                {
+                    'values': dict_values[asset_name_to_category[asset_name]][asset_name]['flow']['index'],
+                    'label': 'Time'
+                },
+            'yAxis':
+                {
+                    'values': dict_values[asset_name_to_category[asset_name]][asset_name]['flow']['data'],
+                    'label': 'Power'
+                },
+            'title': asset_name
+        }
+        for asset_name in asset_name_list
+    ]
+
+    return JsonResponse(results_json, status=200, content_type='application/json', safe=False)
+
+
+@login_required
+@require_http_methods(["GET"])
 def scenario_visualize_results(request, scen_id):
     scenario = get_object_or_404(Scenario, pk=scen_id)
 
     if scenario.project.user != request.user:
         return HttpResponseForbidden()
 
-    if request.method == "GET":
-        with open('static/tempFiles/json_with_results.json') as json_file:
-            dict_values = json.load(json_file)
-        test_data = dict_values['energyProduction']['DSO_consumption']['flow']['data']
+    return render(request, 'scenario/scenario_visualize_results.html',
+                  {'scenario_id': scen_id})
 
-        # TODO: Take time from simulation settings
-        test_times = dict_values['energyProduction']['DSO_consumption']['flow']['index']
 
-        return render(request, 'scenario/scenario_visualize_results.html',
-                      {'scenario_id': scen_id, 'tData': test_data, 'tTime': test_times})
-    elif request.method == "POST" and request.is_ajax():
-        pass
+def find_parent_keys(d, target_key, parent_key=None):
+    for k, v in d.items():
+        if k == target_key:
+            yield parent_key
+        if isinstance(v, dict):
+            for res in find_parent_keys(v, target_key, k):
+                yield res
