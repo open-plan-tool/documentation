a0306e83bb6c40abbc2f02a0d9d85d3189f1d646
Bachibouzouk <pierre-francois.duc@netplus.ch>
2021-11-19 13:39:41 +0100

    Create a new service to send emails

--- app/projects/services.py
+++ app/projects/services.py
@@ -1,3 +1,5 @@
+import logging
+import os
 from django_q.models import Schedule
 from django.contrib import messages
 from django.urls import reverse
@@ -7,9 +9,18 @@ from projects.models import Simulation
 from projects.requests import fetch_mvs_simulation_status
 from projects.constants import PENDING
 from concurrent.futures import ThreadPoolExecutor
-import logging
+from exchangelib import Account, Credentials, Mailbox, Message, EWSTimeZone
+
 
 logger = logging.getLogger(__name__)
+
+# email account which will send the feedback emails
+EXCHANGE_ACCOUNT = os.getenv("EXCHANGE_ACCOUNT")
+EXCHANGE_PW = os.getenv("EXCHANGE_PW")
+EXCHANGE_EMAIL = os.getenv("EXCHANGE_EMAIL")
+# email addresses to which the feedback emails will be sent
+RECIPIENTS = os.getenv("RECIPIENTS").split(",")
+
 
 r"""Functions meant to be powered by Django-Q.
 
@@ -65,6 +76,27 @@ def create_or_delete_simulation_scheduler(**kwargs):
             return False
 
 
+def send_feedback_email(subject, body):
+    tz = EWSTimeZone('Europe/Copenhagen')
+    try:
+        credentials = Credentials(EXCHANGE_ACCOUNT, EXCHANGE_PW)
+        account = Account(
+            EXCHANGE_EMAIL, credentials=credentials, autodiscover=True, default_timezone=tz
+        )
+        recipients = [
+            Mailbox(email_address=recipient) for recipient in RECIPIENTS
+        ]
+        mail = Message(
+            account=account,
+            folder=account.sent,
+            subject=subject,
+            body=body,
+            to_recipients=recipients,
+        )
+        mail.send_and_save()
+    except Exception as ex:
+        logger.warning(f"Couldn't send feedback email. Exception raised: {ex}.")
+        raise ex
 
 
 def excuses_design_under_development(request, link=False):
